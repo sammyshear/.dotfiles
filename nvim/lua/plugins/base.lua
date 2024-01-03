0ce7aa40ed379d9e6a7bf1859e9625bfa51133d0
Sammy Shear <sammyshear1@gmail.com>
2024-01-02 21:45:01 -0500

    Re-added tmux navigator

--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -176,4 +176,22 @@ return {
 		},
 		build = ':TSUpdate',
 	},
+
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	}
 }
