c8d9e8573e71c54d324a4e0e058620a709bcb717
Sammy Shear <sammyshear1@gmail.com>
2024-06-25 05:25:09 -0400

    nvim chesslsp fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,13 +3,28 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        chesslsp = {
-          cmd = {
-            "/home/sammyshear/Coding/chesslsp/chesslsp",
-          },
-          filetypes = { "pgn" },
-          single_file_support = true,
-        },
+        chesslsp = {},
+      },
+      setup = {
+        chesslsp = function(_, opts)
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+
+          if not configs.chesslsp then
+            configs.chesslsp = {
+              default_config = {
+                cmd = {
+                  "/home/sammyshear/Coding/chesslsp/chesslsp",
+                },
+                filetypes = {
+                  "pgn",
+                },
+                single_file_support = true,
+              },
+            }
+          end
+          lspconfig.chesslsp.setup(opts)
+        end,
       },
     },
   },
